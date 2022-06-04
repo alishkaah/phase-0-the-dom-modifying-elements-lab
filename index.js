95cd51f754375d2beef269b997b73eda44f9f993
Moringa <alinoorshukri8@gmail.com>
2022-06-04 09:58:06 +0300

    commit tree b57df3889d74a3f5ee03d0a1c40e5c655d7e0d7e

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+// practice
+const element = document.querySelector('main');
+element.remove();
+
+
+// const element = document.createElement("div");
+// document.body.append(element);
+// const main = document.getElementById("main");
+// main.innerHTML =
+//   "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Alishka is the champion'; 
